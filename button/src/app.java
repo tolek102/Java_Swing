d1cc7b6c4ec0a60de894ff6a474f179e23176888
Pawel Bachta <bachta.pawel@gmail.com>
2018-03-18 09:04:54 +0100

    initial

---
+++ button/src/app.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.GroupLayout;
+/*
+ * Created by JFormDesigner on Sat Oct 14 13:30:15 CEST 2017
+ */
+
+
+
+/**
+ * @author unknown
+ */
+public class app extends JFrame {
+    public app() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        Container contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
---
+++ button/src/dgfhd.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class dgfhd {
+    private JPanel panel1;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
